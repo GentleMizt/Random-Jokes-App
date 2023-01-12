6b40bd64bafb65d27ec26725af17a74ecbe60e4c
GentleMizt <olamideiyanda18@gmail.com>
2023-01-12 15:52:52 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+    let btn = document.querySelector('.click');
+    let container = document.querySelector('.container')
+
+    const getJoke = () =>{
+
+    }
+    
+    btn.addEventListener('click', );
+
