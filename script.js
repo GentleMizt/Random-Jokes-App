c16dcde8e0864822601e003ce5c3fc41983cdb93
GentleMizt <olamideiyanda18@gmail.com>
2023-01-12 16:29:21 +0100

    using .then and .catch

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
     let btn = document.querySelector('.click');
     let container = document.querySelector('.container')
 
-    const getJoke = async() =>{
-        try{
-            let data = await (await fetch('https://api.chucknorris.io/jokes/random')).json();
-            let joke = data.value;
-            container.textContent = joke;
-        } catch(error){
-            console.log(error);
-        }
+    // const getJoke = async() =>{
+    //     try{
+    //         let data = await (await fetch('https://api.chucknorris.io/jokes/random')).json();
+    //         let joke = data.value;
+    //         container.textContent = joke;
+    //     } catch(error){
+    //         console.log(error);
+    //     }
+    // }
+
+
+    // USING .then and .catch
+    const getJoke = () =>{
+        fetch('https://api.chucknorris.io/jokes/random').then();
     }
 
     btn.addEventListener('click', getJoke);
