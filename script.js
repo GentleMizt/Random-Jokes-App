b81170bbaa6197831f02bb494a25f83208684610
GentleMizt <olamideiyanda18@gmail.com>
2023-01-13 14:04:13 +0100

    revision done!

--- script.js
+++ script.js
@@ -13,14 +13,21 @@
 
 
     // USING .then and .catch
-    const getJoke = () =>{
-        fetch('https://api.chucknorris.io/jokes/random').then(data =>{
-            return data.json();
-        }).then(joke => {
-            container.textContent = joke.value;
-        }).catch(error => {
-            console.log(error)
-        });
+    // const getJoke = () =>{
+    //     fetch('https://api.chucknorris.io/jokes/randomdextContent = joke.value;
+    //     }).catch(error => {
+    //         console.log(error)
+    //     });
+    // }
+
+    const getJoke = async() =>{
+        try {
+            let infos = await (await fetch('https://api.chucknorris.io/jokes/random')).json();
+            let joke = infos.value;
+            container.textContent = joke;
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     btn.addEventListener('click', getJoke);
